54e414def7089427f939d1ab921c3d4ce05d042c
Gergely Nemeth <gergo@secretsaucepartners.com>
2014-03-08 07:14:44 +0100

    Functional Bences part

--- js/config-functional-javascript.js
+++ js/config-functional-javascript.js
@@ -24,6 +24,64 @@ var quizJSON = {
       ],
       "correct": "<p><span>That's right!</span></p>",
       "incorrect": "<p><span>Uhh no.</span></p>"
+    },
+    {
+      "r": {
+        "app.js": ["function magic(arr) {",
+          "  return arr.length > 1 ? magic(arr.slice(1)) : arr[0];",
+          "}",
+          "console.log(magic([1,2,3,4,5,6]));"].join('<br/>')
+      },
+      "q": "node app.js",
+      "a": [
+        {"option": "1", "correct": false},
+        {"option": "[]", "correct": false},
+        {"option": "6", "correct": true},
+        {"option": [1,2,3,4,5,6], "correct": false}
+      ],
+      "correct": "<p><span>That's right!</span></p>",
+      "incorrect": "<p><span>Uhh no.</span></p>"
+    },
+    {
+      "r": {
+        "app.js": ["function doge(wow) {",
+                    "  return function(very) {",
+                    "    return very.call(wow);",
+                    "  }",
+                    "}",
+
+                    "var such = { x: 2 };",
+                    "such.much = doge({ x: 1});",
+
+                    "console.log(such.much(function() {",
+                    "  return this.x;",
+                    "}))"].join('<br/>')
+      },
+      "q": "node app.js",
+      "a": [
+        {"option": "1", "correct": true},
+        {"option": "2", "correct": false},
+        {"option": "undefined", "correct": false}
+      ],
+      "correct": "<p><span>That's right!</span></p>",
+      "incorrect": "<p><span>Uhh no.</span></p>"
+    },
+    {
+      "r": {
+        "app.js": ["function fizz(buzz, fizzbuzz) {",
+          "   return this[buzz];",
+          "}",
+          "var fuzz = [3,2,1,0];",
+          "console.log(fuzz.map(fizz.bind(fuzz)));"].join('<br/>')
+      },
+      "q": "node app.js",
+      "a": [
+        {"option": "[0,1,2,3]", "correct": true},
+        {"option": "[3,2,1,0]", "correct": false},
+        {"option": "[undefined, undefined, undefined, undefined]", "correct": false}
+      ],
+      "correct": "<p><span>That's right!</span></p>",
+      "incorrect": "<p><span>Uhh no.</span></p>"
     }
   ]
 };
